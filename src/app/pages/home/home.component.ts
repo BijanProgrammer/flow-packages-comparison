87dd82d95dc8c749f5e0b9c6fa9e2237e6731a44
BijanProgrammer <bijaneisapour@gmail.com>
2022-09-04 14:58:56 +0430

    [INIT]

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,38 @@
 import {Component} from '@angular/core';
 
+enum Score {
+    GOOD = 'good',
+    OK = 'ok',
+    BAD = 'bad',
+}
+
+interface Row {
+    name: string;
+    url: string;
+    angularSupport: Score;
+    documents: Score;
+    customizableNodes: Score;
+    customizableEdges: Score;
+    animation: Score;
+    ui: Score;
+}
+
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+    public rows: Row[] = [
+        {
+            name: 'Package',
+            url: 'https://www.totaljs.com/',
+            angularSupport: Score.GOOD,
+            documents: Score.OK,
+            customizableNodes: Score.BAD,
+            customizableEdges: Score.OK,
+            animation: Score.OK,
+            ui: Score.OK,
+        },
+    ];
+}
